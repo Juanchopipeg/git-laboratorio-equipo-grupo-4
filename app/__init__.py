5c8542edbb1b83f16036b6eb6d004ec694b7ca15
Pablo Nieto <juanpablonie77@gmail.com>
2026-09-18 13:28:52 -0500

    Implementar creación y listado de pacientes

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-    from app.models.paciente import Paciente
-  
+
     app = Flask(__name__)
     app.config.from_object("config.Config")
 
     db.init_app(app)
+
+    from app.models.paciente import Paciente
     from app.routes.paciente_routes import paciente_bp
+
     app.register_blueprint(paciente_bp)
-    
+
     @app.route("/")
     def inicio():
         return "Sistema de Citas Médicas funcionando"
--- app/routes/paciente_routes.py
+++ app/routes/paciente_routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from app.models.paciente import Paciente
+from app import db
 
 paciente_bp = Blueprint("paciente", __name__)
 
@@ -10,3 +11,23 @@ def listar_pacientes():
         "pacientes.html",
         pacientes=pacientes
     )
+
+@paciente_bp.route("/pacientes/nuevo", methods=["GET", "POST"])
+def nuevo_paciente():
+
+    if request.method == "POST":
+
+        paciente = Paciente(
+            nombre=request.form["nombre"],
+            apellido=request.form["apellido"],
+            documento=request.form["documento"],
+            telefono=request.form["telefono"],
+            correo=request.form["correo"]
+        )
+
+        db.session.add(paciente)
+        db.session.commit()
+
+        return redirect(url_for("paciente.listar_pacientes"))
+
+    return render_template("nuevo_paciente.html")
